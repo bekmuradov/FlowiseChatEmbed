39f95941304708ee7f753f14ea8a3b60e87a42ec
Beck Bekmyradov <47065940+bekmuradov@users.noreply.github.com>
2023-11-29 02:04:10 +0400

    Update default color

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,7 +51,7 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    }}>Assistant by
       <a
         ref={liteBadge}
         href={'https://www.mixsoft.ai/'}
